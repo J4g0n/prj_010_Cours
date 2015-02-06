8db96bf9a6191ac4b330eb87fe5dfe9f6281e3bb
J4g0n <simon.andreux@gmail.com>
2015-02-06 15:54:50 +0100

    commit

---
+++ src/package_8_ExercicesSWING/Exercice_0940/Exercice_0930/Exercice_0940.java
@@ -0,0 +1,9 @@
+package package_8_ExercicesSWING.Exercice_0940.Exercice_0930;
+
+
+public class Exercice_0940 {
+    public static void main (String [] args) {
+        Win_0940_10 f2 = new Win_0940_10();
+        f2.setVisible(true);
+    }
+}
